bee63b1e0d90f0fd83106b545816020aea0b75be
Miles <mae9775@nyu.edu>
2022-04-29 15:04:30 -0400

    working on third room (projectiles)

---
+++ Assets/scripts/projectileSpawn.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileSpawn : MonoBehaviour
+{
+    public GameObject projectile; 
+
+    // coroutine projectile spawning here
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/transitionObj.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transitionObj : MonoBehaviour
+{
+    
+    void Update()
+    {
+         void endAnim()
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
