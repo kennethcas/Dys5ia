952576a81463f0ff94646f35c0f03898acf6c9eb
Miles <mae9775@nyu.edu>
2022-04-29 23:43:11 -0400

    workin on projectilesagain tripping balls wile i tipe this

---
+++ Assets/scripts/projectileDestroy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileDestroy : MonoBehaviour
+{
+    void selfDestruct()
+    {
+        Destroy(gameObject);
+    }
+ 
+}
