d8ce43c554dfbfaeb25534fe48edb2e5a701def9
kenneth <kc4338@nyu.edu>
2022-05-04 17:38:22 -0400

    level 2 part 3 done

--- Assets/scripts/singleTrigger.cs
+++ Assets/scripts/singleTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class singleTrigger : MonoBehaviour
 {
@@ -9,16 +10,44 @@ public class singleTrigger : MonoBehaviour
     public GameObject triggerObj;
     [Header("Visual Cues")]
     public GameObject cueObj;
+    [Header("Timer")]
+    public float timer;
+    public bool timerStarted;
+    
 
     private void Awake()
     {
         collision = GetComponent<BoxCollider>();
         cueObj.SetActive(false);
+        timerStarted = false;
+    }
+
+    private void Update()
+    {
+        Debug.Log(timer);
+        if (timerStarted)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("colliding with trigger obj");
+        //Debug.Log("colliding with trigger obj");
         cueObj.SetActive(true);
+        timerStarted = true;
+
+        //timer -= Time.deltaTime;
+
     }
+
+   /* public void startTimer()
+    {
+        timer -= Time.deltaTime;
+    }*/
 }
